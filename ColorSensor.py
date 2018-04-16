8c65a2a901aaa5d1bbcf145a1ebb63c6f3eb9f58
Akli Amrous <33397020+akliam77@users.noreply.github.com>
2018-04-16 16:47:39 -0700

    Converted the code into a class

--- ColorSensor.py
+++ ColorSensor.py
@@ -1,5 +1,4 @@
 #This is the Python program to detect color during the FRC 2018 competition using the Adafruit TCS 34725 color sensor and the raspberry pi 3
-
 #import necessary dependencies
 from networktables import NetworkTables as nt
 import time
@@ -11,41 +10,47 @@ import os
 #IP address and SD tables
 #Initialize the Server as well.
 '''-----------------------------------------------------------------------'''
-ip = "10.46.82.2"
-nt.initialize(server=ip)
-sc = nt.getTable('Scale')
-sw = nt.getTable('Switch')
+class Sensor(object):
+    def __init__(self, ip, switch, scale):
+        
+        self.ip = "XX.XX.XX.X"
+        
+        self.scale = nt.getTable('Scale')
+        self.switch = nt.getTable('Switch')
 '''------------------------------------------------------------------------'''
 #Create an instance of the Color Sensor
-sensor = af.TCS34725()
+        self.sensor = af.TCS34725()
 
-sensor.set_interrupt(False)
+        #sensor.set_interrupt(False)
 
 #Detect Color and Calculate Lumosity in infinite loop
 '''------------------------------------------------------------------'''
-while(1):
+
+    def scan():
+        
+        while(1):
     
-    r, g, b, c = sensor.get_raw_data()
-    time.sleep(0.5)
-    print("rgb=("+str(r)+", "+str(g)+", "+str(b)+")")
+            r, g, b, c = self.sensor.get_raw_data()
+            time.sleep(0.5)
+            print("rgb=("+str(r)+", "+str(g)+", "+str(b)+")")
     
-    colorTemp = af.calculate_color_temperature(r, g,b)
-    lux = af.calculate_lux(r,g,b)
-    print(str(colorTemp) + "k")
-    print(str(lux) + " lux")
-    if lux <= 9:
-        sc.putNumber('View', 0)
-        print("Lumosity is %s, Robot is not in the Scale Area" % (str(lux)))
-    elif lux =< 10:
-        sc.putNumber('View', 1)
-        print("Lumosity is %s, Robot is in the Scale Area" % ((str(lux)))
+            colorTemp = af.calculate_color_temperature(r, g,b)
+            lux = af.calculate_lux(r,g,b)
+        print(str(colorTemp) + "k")
+        print(str(lux) + " lux")
+        if lux <= 9:
+            self.scale.putNumber('View', 0)
+            print("Lumosity is %s, Robot is not in the Scale Area" % (str(lux)))
+        elif lux =< 10:
+            self.scale.putNumber('View', 1)
+            print("Lumosity is %s, Robot is in the Scale Area" % ((str(lux)))
 '''---------------------------------------------------------------------'''
               
               
 '''-----------------------------------------------------------------------'''
               #Enter sleep mode for Sensor
-sensor.set_interrupt(True)
-sensor.disable()
+        sensor.set_interrupt(True)
+        sensor.disable()
 
 
 
